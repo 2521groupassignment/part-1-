e3a1e592c8232b21100d8f7062968141f7ceabe9
sarahilwil <49468042+sarahilwil@users.noreply.github.com>
2019-04-26 11:07:08 +1000

    Update Dijkstra.c

--- Dijkstra.c
+++ Dijkstra.c
@@ -9,8 +9,8 @@
 // define a very big number to set all initial dist to
 #define INF 0x7FFFFFFF
 
-// need to check the order nodes get added to the lists (add to head or tail
-// of the list??)
+// auxillary function
+static void add_pred(ShortestPaths *path, Vertex w, Vertex v);
 
 ShortestPaths dijkstra(Graph g, Vertex v) {
     
@@ -19,20 +19,20 @@ ShortestPaths dijkstra(Graph g, Vertex v) {
     // we know so far
     ShortestPaths *short_p = malloc(sizeof(ShortestPaths));
     short_p->src = v;
-    short_p->noNodes = numVertices(g);   
+    short_p->noNodes = numVerticies(g);   
 	// initialise the distance array
-	short_p->dist = malloc(numVertices(g) * sizeof(int));
+	short_p->dist = malloc(numVerticies(g) * sizeof(int));
 	assert(short_p->dist != NULL);
 	// initialise the predecessor array
-	short_p->pred = malloc(numVertices(g) * sizeof(PredNode *));
+	short_p->pred = malloc(numVerticies(g) * sizeof(PredNode *));
 	assert(short_p->pred != NULL);
 	
 	
 	// initialise an array to hold the nodes that have been visited in the graph
-	int seen_set[numVertices(g)];
+	int seen_set[numVerticies(g)];
 	
 	// assign the necessary values to the variables in the arrays
-	for( int i = 0; i < numVertices(g); i++) {
+	for( int i = 0; i < numVerticies(g); i++) {
 	    // initial dist from src for all nodes in infinity
 	    short_p->dist[i] = INF;
 	    // the pred for each node is currently unknown, so set to NULL
@@ -42,14 +42,14 @@ ShortestPaths dijkstra(Graph g, Vertex v) {
 	}
 
 	// we already known the distance to the src is 0
-	short_p->dist[src] = 0;
+	short_p->dist[v] = 0;
 	
 	// set up a new priority queue which will aid in traversing graph g
 	PQ vertex_set = newPQ();
 	// create a new pqnode, type ItemPQ and set its values
 	ItemPQ new_pqnode;
-	new_pqnode.key = src;
-	new_pqnode.value = short_p->dist[src];
+	new_pqnode.key = v;
+	new_pqnode.value = short_p->dist[v];
 	addPQ(vertex_set, new_pqnode);
 	
 	
@@ -60,11 +60,11 @@ ShortestPaths dijkstra(Graph g, Vertex v) {
 	    // dequeue the item with the smallest distance
 	    ItemPQ curr_node = dequeuePQ(vertex_set);
 	    // ensure the vertex has not been seen before
-	    if(seen[curr_node.key] != -1) continue;
+	    if(seen_set[curr_node.key] != -1) continue;
 	    
 	    // then change the unseen vertex to the seen set, by changing
 	    // its value in the array to 1 (this could be any value)
-	    seen[traversal_node.key] = 1;
+	    seen_set[curr_node.key] = 1;
 	    
 	    // get all the neighbours of the current node
 	    AdjList neighbours = outIncident(g, curr_node.key);
@@ -86,11 +86,14 @@ ShortestPaths dijkstra(Graph g, Vertex v) {
 	            short_p->pred[neighbours->w] = malloc(sizeof(PredNode));
 	            short_p->pred[neighbours->w]->v = curr_node.key;
 	            short_p->pred[neighbours->w]->next = NULL;
-	        }else if((short_p->dist[curr_node.key] + neighbours->weight) == short_p->dist[neighbours->w]){
+	        } else if((short_p->dist[curr_node.key] + neighbours->weight) == short_p->dist[neighbours->w]){
 	            // update the distance array
 	            short_p->dist[neighbours->w] = (short_p->dist[curr_node.key] + neighbours->weight);
 	            // update the pred list
-	            // WORK ON THIS 
+	            add_pred(short_p, neighbours->w, curr_node.key);
+	            
+	            
+	            
 	        }
 	        new_pqnode.key = neighbours->w;
 	        new_pqnode.value = neighbours->weight;
@@ -104,13 +107,15 @@ ShortestPaths dijkstra(Graph g, Vertex v) {
 	// case: distances to some nodes are still set to inf
 	// need to make them equal to 0
 	int k;
-	for(k = 0; k < numVertices(g); k++){
+	for(k = 0; k < numVerticies(g); k++){
 	    if(short_p->dist[k] == INF) {
 	        short_p->dist[k] = 0;
 	    }
 	}
 	
-	return shortest_path;
+	free(seen_set);
+
+	return *short_p;
 }
 
 void showShortestPaths(ShortestPaths paths) {
@@ -123,7 +128,14 @@ void showShortestPaths(ShortestPaths paths) {
     printf("\n");
     
     // print the pred list
-        
+    
+    PredNode *curr = paths.pred[paths.src];
+    while(curr != NULL){
+        printf("%d, ", curr->v);
+        curr = curr->next;
+    }
+    printf("\n");
+      
     
 
 }
@@ -133,4 +145,20 @@ void  freeShortestPaths(ShortestPaths paths) {
 
     free(paths.pred);
     free(paths.dist);
+}
+
+static void add_pred(ShortestPaths *path, Vertex w, Vertex v){
+    
+    PredNode *new = malloc(sizeof(PredNode));
+    new->v = v;
+    new->next = NULL;
+    
+    PredNode *curr = path->pred[w];
+    
+    while(path->pred[w]->next != NULL) {
+        path->pred[w] = path->pred[w]->next;
+    }
+    path->pred[w]->next = new;
+    path->pred[w] = curr;
+
 }
