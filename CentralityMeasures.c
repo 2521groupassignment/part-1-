0df3a80ffb58abba5f6326abb9c6b315aeb956fd
sarahilwil <49468042+sarahilwil@users.noreply.github.com>
2019-04-27 21:24:19 +1000

    style fixed for current parts, passes degree tests

--- CentralityMeasures.c
+++ CentralityMeasures.c
@@ -1,30 +1,38 @@
 // Graph ADT interface for Ass2 (COMP2521)
+// Part 3 of Assignment 2
+// Jing Jing Fan and Sarah Williams 
+
 #include "CentralityMeasures.h"
 #include "Dijkstra.h"
 #include "PQ.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include "Graph.h"
+#include <assert.h>
 
 // Auxillary functions
-static double shortPathDistance (Graph g, Vertex v); 
-static double numReachable (Graph G, Vertex v);
+static int countInAdjVs(Graph g, Vertex v);
+static int countOutAdjVs(Graph g, Vertex); 
 
-int countInAdjVs(Graph g, Vertex v) 
-{
+static double shortPathDistance(Graph g, Vertex v); 
+static double numReachable(Graph G, Vertex v);
+
+// private function that counts the numer of verticies that are directed
+// towards the current vertex 
+static int countInAdjVs(Graph g, Vertex v) {
     double i = 0;
     AdjList curr = inIncident(g, v);
     while (curr != NULL) {
         i++;
         curr = curr->next;
-    }
-    //printf("hello %f\n", i);
-    
+    } 
+       
     return i;
 
 }
 
-int countOutAdjVs(Graph g, Vertex v) {
+// private function that counts the number of verticies that are directed 
+// out from the current vertex 
+static int countOutAdjVs(Graph g, Vertex v) {
 
     double i = 0;
     AdjList curr = outIncident(g, v);
@@ -33,17 +41,16 @@ int countOutAdjVs(Graph g, Vertex v) {
         curr = curr->next;
     }
     
-    //printf("hello %d\n", i);
     return i;
 
 }
 
+
 NodeValues outDegreeCentrality(Graph g){
+
 	NodeValues GraphOutDegrees = {0};
 	int nV = numVerticies(g);
-   
     GraphOutDegrees.noNodes = nV;
-    
     GraphOutDegrees.values = calloc(nV, sizeof(double));
     
     Vertex v;
@@ -55,6 +62,7 @@ NodeValues outDegreeCentrality(Graph g){
     
     return GraphOutDegrees;
 }
+
 NodeValues inDegreeCentrality(Graph g){
 	NodeValues GraphInDegrees = {0};
 	int nV = numVerticies(g);
@@ -73,10 +81,11 @@ NodeValues inDegreeCentrality(Graph g){
     return GraphInDegrees;
 
 }
+
 NodeValues degreeCentrality(Graph g) {
     NodeValues GraphDegrees = {0};
    
-    /*int nV = numVerticies(g);
+    int nV = numVerticies(g);
     GraphDegrees.noNodes = nV;
     GraphDegrees.values = calloc(nV, sizeof(double));
     
@@ -85,7 +94,7 @@ NodeValues degreeCentrality(Graph g) {
     while (v < nV) {
         GraphDegrees.values[v] = countOutAdjVs(g, v) + countInAdjVs(g, v);
         v++;
-    }*/
+    }
 
     return GraphDegrees;
 
@@ -93,7 +102,7 @@ NodeValues degreeCentrality(Graph g) {
 
 NodeValues closenessCentrality(Graph g){
 
-    assert(g != NULL);
+    //assert(g != NULL);
 
     NodeValues GraphCloseness = {0};
        
@@ -102,54 +111,61 @@ NodeValues closenessCentrality(Graph g){
 	GraphCloseness.values = calloc(nV,sizeof(double));
 	    
     Vertex v;
-    double num_reach;
+    double numReach;
     
     // for each of the nodes in the graph need to perform the Wasserman & Faust
     // equation
     
     for( v = 0; v < nV; v++){
     
-        num_reach = numReachable(g,v);
-        double numerator = (num_reach - 1) * (num_reach - 1);
-        double denominator = (numVerticies(g)-1)*( shortPathDistance(g, v));
+        numReach = numReachable(g,v);
+        double numerator = (numReach - 1) * (numReach - 1);
+        double denominator = ((nV)-1)*( shortPathDistance(g, v));
         GraphCloseness.values[v] = (numerator / denominator);
-        
-        if(num_reach == 0) {
+        if(numReach == 0) {
             GraphCloseness.values[v] = 0;
         }
     }
 	
-    return GraphCloseness;
+	
+	return GraphCloseness;
 	
 }
 
+// private function that finds the sum of the distances from vertex v to 
+// all other reachable nodes
 static double shortPathDistance(Graph g, Vertex v){
 
     double shortDistance = 0;
     
     ShortestPaths path;
     path = dijkstra(g, v);
-    for(Vertex i = 0; i < numVerticies(g); i++){
-        shortDistance += path.dist[i];
+    
+    for(int i = 0; i < numVerticies(g); i++){
+        shortDistance = shortDistance + path.dist[i];
     }
    
    return shortDistance; 
     
 }
 
+// private function that finds the number of nodes reachable from the vertex v
 static double numReachable (Graph g, Vertex v){
+
     ShortestPaths path = dijkstra(g, v);
-    double n_reachable = 0;
+    double nReachable = 0;
     
     for( int i = 0; i < numVerticies(g); i++){
         if(path.dist[i] != 0){
-            n_reachable++;
+            nReachable++;
         }
     }
     
-    return n_reachable;
+    return nReachable;
     
 }
+
+
 
 NodeValues betweennessCentrality(Graph g){
 	NodeValues throwAway = {0};
